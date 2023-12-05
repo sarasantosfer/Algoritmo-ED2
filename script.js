0ee1e3cbf39857eff366d7feff0936a6ffd81881
sarasantosfer <116079275+sarasantosfer@users.noreply.github.com>
2023-12-05 12:20:35 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let vetorInicial=[ 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59]
+let vetorFinal = [];
+for(let i = 0; i < vetorInicial.length; i++){
+   
+    vetorInicial[i] % 2 == 1
+    vetorFinal.push(vetorInicial[i])
+}
+
+console.log(vetorFinal)
